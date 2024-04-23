e1c95c58d2348c99328ae02698e9925c1c8c13f5
Erin Muzughi <52017685+erinmuzughi@users.noreply.github.com>
2024-04-23 08:57:40 -0500

    first commit

---
+++ server/src/test/java/com/example/portfoliosite/PortfolioSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfoliosite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
