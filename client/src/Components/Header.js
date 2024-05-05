88535af7c3be921be9c123c66a5a796f1e163e3a
Brittany Robinson <bncopple@gmail.com>
2024-05-05 09:39:40 -0500

    updated header

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -7,9 +7,8 @@ export default function header(){
         <>
         <a class="header-home" href="/">Home</a>
         <button class="header-homeButton" type="button"></button>
-
         <a class="header-aboutme" href="/AboutMe">About Me</a>
         <button class="header-aboutmeButton" type="button"></button>
-        </>
+        </>                 
     )
 }
