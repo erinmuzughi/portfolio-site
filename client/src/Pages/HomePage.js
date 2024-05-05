3395d8936a74ee5d865627451ec8d4d45f1012c0
Brittany Robinson <bncopple@gmail.com>
2024-05-05 09:42:40 -0500

    commented out header

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -5,7 +5,7 @@ export default function homePage(){
     return (
         <>
         // ToDo: import Header component when made
-        <Header/>
+       // <Header/>
         <div className='homePageWriting'>
             <h1>Writes Code</h1>
         </div>
