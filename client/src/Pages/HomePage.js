161370ae415b4105e7d7d348e456373524cab4e4
Brittany Robinson <bncopple@gmail.com>
2024-05-25 07:58:15 -0500

    added default mui styling to admin sign in page

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -4,9 +4,7 @@ export default function homePage(){
 
     return (
         <>
-        {/* // ToDo: import Header component when made
-       // <Header/> */}
-
+        <Header/>
         <div className='homePageWriting'>
             <h1>Writes Code</h1>
         </div>
