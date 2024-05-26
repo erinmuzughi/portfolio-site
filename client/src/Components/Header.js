c5635cdec51126d7128e6cb2673f46e267e09186
Brittany Robinson <136764039+bnRobinson@users.noreply.github.com>
2024-05-26 07:49:22 -0500

    Update Header.js

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = ['About Me', 'Experience', 'Projects', 'Contact'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 export default function Header() {
