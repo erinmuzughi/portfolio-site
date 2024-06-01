82dcebd9169b86c698c2ca33aae356459aedd0a7
Brittany Robinson <bncopple@gmail.com>
2024-06-01 07:32:13 -0500

    need to add logic for updating the Experience Data file

--- client/src/Components/UpdateExperienceForm.js
+++ client/src/Components/UpdateExperienceForm.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 export default function updateExperienceForm(){
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const handleExperiencechange = (e) => {
+        //update the experienceData file with new entry upon  submission
+      };
+    
+
   return(
     <>
+    <form onSubmit={handleSubmit}>
+        <h2>Update Your Experience</h2>
+        <input type="text" value= {newExperience} onChange={handleExperiencechange}></input>
+
+
+    </form>
     </>
   )
 }
