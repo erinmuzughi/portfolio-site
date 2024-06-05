2506d75349304c097472d8397d60274af8f588b3
Erin Muzughi <52017685+erinmuzughi@users.noreply.github.com>
2024-06-05 12:29:51 -0500

    fixed a few typos and left a comment with my debugging find which is the same as my text

--- client/src/Components/UpdateProjectsForm.js
+++ client/src/Components/UpdateProjectsForm.js
@@ -9,8 +9,8 @@ export default function UpdateProjetsForm(){
   const [title, setTitle]= useState('');
     const [subtitle, setSubtitle]= useState('');
     const [image, setImage]= useState('');
-    const [altText, setAlttext]= useState('');
-    const [hyperLink, setHyperLink]= useState('');
+    const [altText, setAltText]= useState(''); //fixed a typo, this was Alttext instead of AltText throughout
+    const [hyperlink, setHyperlink]= useState(''); //fixed 'hyperLink' and HyperLink to hyperlink and Hyperlink thorughout for consistency with the data file -- hyperlink is one word not two
 
 
    const handleTitleChange= (e) => {
@@ -26,32 +26,42 @@ export default function UpdateProjetsForm(){
  };
 
  const handleAltTextChange= (e) => {
-  setAlttext(e.target.value);
+  setAltText(e.target.value);
 };
 
-const handleHyperLinkChange= (e) => {
-  setHyperLink(e.target.value);
+const handleHyperlinkChange= (e) => {
+  setHyperlink(e.target.value);
 };
 
 const handleSubmit = (e) => {
 
   e.preventDefault();
 
-  const project= {title, subtitle, image, altText, hyperLink};
+  const project= {title, subtitle, image, altText, hyperlink};
 
   console.log(project);
+  console.log('Updated projects array:', projects); //added this console log in two places, so it is adding it to the array of projects, the problem is we haven't created persistent data yet so it's wiped every time we restart the app and doesn't actually get added to the .js file itself. We need to make the data persistent, which is where the database comes in. So we actually 
 
   const newProject= {
     title: project.title,
     subtitle: project.subtitle, 
     image: project.image, 
     altText: project.altText, 
-    hyperLink: project.hyperLink
+    hyperlink: project.hyperlink
   }
 
   projects.push(newProject);
 
-  console.log(projects)
+  console.log(projects);
+  console.log('Updated projects array:', projects);
+
+  const updatedProjects = [...projects, newProject];
+
+  // Save to local storage
+  localStorage.setItem('projects', JSON.stringify(updatedProjects));
+
+  console.log('New project added:', newProject);
+  console.log('Updated projects array:', updatedProjects);
 
 }
 
@@ -63,7 +73,7 @@ const handleSubmit = (e) => {
         <input type="text" value= {subtitle} onChange={handleSubtitleChange} placeholder="Subtitle"></input>
         <input type="text" value= {image} onChange={handleImageChange} placeholder="Image"></input>
         <input type="text" value= {altText} onChange={handleAltTextChange} placeholder="AltText"></input>
-        <input type="text" value= {hyperLink} onChange={handleHyperLinkChange} placeholder="HyperLink"></input>
+        <input type="text" value= {hyperlink} onChange={handleHyperlinkChange} placeholder="HyperLink"></input>
         <button type="submit" >Save</button>
 
     </form>
