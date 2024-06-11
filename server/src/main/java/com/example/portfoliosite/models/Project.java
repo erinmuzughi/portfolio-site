ce550d9bf7fa281d0bb7db8efd0a91d075734e55
Brittany Robinson <bncopple@gmail.com>
2024-06-11 11:16:27 -0500

    created models for experience, education and project.

---
+++ server/src/main/java/com/example/portfoliosite/models/Project.java
@@ -0,0 +1,50 @@
+package com.example.portfoliosite.models;
+
+public class Project {
+
+    private String title;
+    private String subtitle;
+    private String image;
+    private String altText;
+    private String hyperlink;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAltText() {
+        return altText;
+    }
+
+    public void setAltText(String altText) {
+        this.altText = altText;
+    }
+
+    public String getHyperlink() {
+        return hyperlink;
+    }
+
+    public void setHyperlink(String hyperlink) {
+        this.hyperlink = hyperlink;
+    }
+}
