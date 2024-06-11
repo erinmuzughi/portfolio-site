8bc5d19be12175b1770cf2796c3ff02ed67c1bef
Brittany Robinson <bncopple@gmail.com>
2024-06-11 11:19:03 -0500

    fields, getters and setters added for model classes

--- server/src/main/java/com/example/portfoliosite/models/Education.java
+++ server/src/main/java/com/example/portfoliosite/models/Education.java
@@ -1,4 +1,23 @@
 package com.example.portfoliosite.models;
 
 public class Education {
+
+    private String title;
+    private String company;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
 }
--- server/src/main/java/com/example/portfoliosite/models/Experience.java
+++ server/src/main/java/com/example/portfoliosite/models/Experience.java
@@ -1,4 +1,51 @@
 package com.example.portfoliosite.models;
 
 public class Experience {
+
+    private String jobTitle;
+
+    private String company;
+    private String startDate;
+    private String endDate;
+    private String description;
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
