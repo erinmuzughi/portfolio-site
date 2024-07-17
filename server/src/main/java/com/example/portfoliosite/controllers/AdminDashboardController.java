be15303f83ba815a877f028dd6ebbd4a2108cc0e
Brittany Robinson <bncopple@gmail.com>
2024-07-17 09:29:08 -0500

    ability to add entries created for education, experience, and project

--- server/src/main/java/com/example/portfoliosite/controllers/AdminDashboardController.java
+++ server/src/main/java/com/example/portfoliosite/controllers/AdminDashboardController.java
@@ -1,9 +1,16 @@
 package com.example.portfoliosite.controllers;
 
 import com.example.portfoliosite.data.EducationRepository;
+import com.example.portfoliosite.data.ExperienceRepository;
+import com.example.portfoliosite.data.ProjectRepository;
+import com.example.portfoliosite.models.Education;
+import com.example.portfoliosite.models.Experience;
+import com.example.portfoliosite.models.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +19,36 @@ public class AdminDashboardController {
 
     @Autowired
     private EducationRepository educationRepository;
+
+    private Education education;
+
+    private ExperienceRepository experienceRepository;
+
+    private Experience experience;
+
+    private ProjectRepository projectRepository;
+
+    private Project project;
+
+   // Education Section
+@PostMapping
+    public Education addEducation(@RequestBody EducationRepository educationRepository) {
+    return educationRepository.save(education);
+}
+
+// Experience Section
+    @PostMapping
+
+    public Experience addExperience (@RequestBody ExperienceRepository experienceRepository) {
+    return experienceRepository.save(experience);
+    }
+
+
+// Project Section
+    @PostMapping
+
+    public Project addProject (@RequestBody ProjectRepository projectRepository) {
+    return projectRepository.save(project);
+    }
 
 }
